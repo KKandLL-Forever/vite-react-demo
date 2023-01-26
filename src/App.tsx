e0614567c40b384774fe8f050cd51d701b3d3fbe
KKandLL-Forever <clay1wang@gmail.com>
2023-01-26 18:23:02 +0800

    fix webstorm tailwind plugin worked

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className={styles.App}>
       <div
-        className={"bg-red-400"}
+        className="bg-red-400"
       >
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
